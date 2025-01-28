ee82ae87aec3f2892a555c982dea074021800d81
Karim Hussein <abobalal.karim@gmail.com>
2025-01-28 19:29:35 +0300

    commit the home page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-base:"/countries/",
+// base:"/countries/",
   plugins: [react()]
 })
 
+// "homepage": "https://karim-abd-hussein.github.io/countries/",
