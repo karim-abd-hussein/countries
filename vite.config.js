f545befd7104eb5a4032c10282ba0633f4db91ef
Karim Hussein <abobalal.karim@gmail.com>
2025-01-28 19:40:20 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-// base:"/countries/",
+base:"/countries/",
   plugins: [react()]
 })
 
-// "homepage": "https://karim-abd-hussein.github.io/countries/",
